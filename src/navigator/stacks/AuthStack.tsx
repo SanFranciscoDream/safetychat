7eee594e2b6a4241644c3424bb48eb5a03c50bae
SanFranciscoDream <sanfrancisco.development@gmail.com>
2021-07-01 17:52:35 +0300

    remove not needed components

--- src/navigator/stacks/AuthStack.tsx
+++ src/navigator/stacks/AuthStack.tsx
@@ -4,8 +4,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { screens } from '../consts/screens';
 
 import AuthAuthentificationScreen from '../../screens/auth/AuthAuthentificationScreen';
-import AuthLoginScreen from '../../screens/auth/AuthLoginScreen';
-import AuthRegistrationScreen from '../../screens/auth/AuthRegistrationScreen';
 import AuthConfarmationScreen from '../../screens/auth/AuthConfarmationScreen';
 
 const Stack = createStackNavigator();
@@ -14,7 +12,5 @@ export const AuthStack = (
   <>
     <Stack.Screen name={screens.AUTH_AUTHENTIFICATION} component={AuthAuthentificationScreen} />
     <Stack.Screen name={screens.AUTH_CONFARMATION} component={AuthConfarmationScreen} />
-    <Stack.Screen name={screens.AUTH_LOGIN} component={AuthLoginScreen} />
-    <Stack.Screen name={screens.AUTH_REGISTRATION} component={AuthRegistrationScreen} />
   </>
 );
