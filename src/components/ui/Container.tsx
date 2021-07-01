b88efa84fac142e47a1cff1d8c6d5c45932500ee
SanFranciscoDream <sanfrancisco.development@gmail.com>
2021-07-01 14:42:26 +0300

    container ui component

---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+import { colors } from '../../styles/colors';
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container = ({ children }: ContainerProps) => {
+  return <ScrollView contentContainerStyle={styles.container}>{children}</ScrollView>;
+};
+
+export default Container;
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: colors.white,
+    paddingHorizontal: 20,
+    paddingTop: 32,
+  },
+});
--- src/screens/auth/AuthAuthentificationScreen.tsx
+++ src/screens/auth/AuthAuthentificationScreen.tsx
@@ -1,45 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
-import Navigation from '../../base/Navigation';
-import { screens } from '../../navigator/consts/screens';
-
-import { colors } from '../../styles/colors';
+import Container from '../../components/ui/Container';
 
 const AuthAuthentification = () => {
-  const goToLogin = () => {
-    Navigation.navigate(screens.AUTH_LOGIN);
-  };
-
-  const goToRegistration = () => {
-    Navigation.navigate(screens.AUTH_REGISTRATION);
-  };
-
-  const goToApp = () => {
-    Navigation.navigate(screens.MAIN_APP);
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.textBody}>THIS IS AUTH_AUTHENTIFICATION SCREEN</Text>
-      <Button title="GO TO LOGIN" onPress={goToLogin} />
-      <Button title="GO TO REGISTRATION" onPress={goToRegistration} />
-      <Button title="GO TO APP" onPress={goToApp} />
-    </View>
+    <Container>
+      <Text>Authentification screen</Text>
+    </Container>
   );
 };
 export default AuthAuthentification;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.white,
-  },
-
-  textBody: {
-    fontSize: 25,
-    fontFamily: 'NotoSansJP-Bold',
-  },
-});
+const styles = StyleSheet.create({});
